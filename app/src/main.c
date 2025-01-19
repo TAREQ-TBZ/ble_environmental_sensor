a992db0d6394e69151bf4b097de7c8884e4b83aa
TAERQ-TBZ <tareq@gmx.net>
2025-01-19 23:44:42 +0100

    app: add minimal zephyr app skeleton

---
+++ app/src/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024 Tareq Mhisen
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+int main(void)
+{
+
+	LOG_INF("Hello :)");
+
+	return 0;
+}
