e0102eaadb17e3edf4266f7b0f8f8b8f8bbc876f
TAREQ-TBZ <tareq@gmx.net>
2025-02-14 02:09:57 +0100

    app: add humidity & temperature service
    This change introduces the humidity and temperature sensor service,
    responsible for interfacing with the sensor. The service provides
    functions to read and process environmental data, which can then be
    transmitted over Zigbee.

--- app/src/main.c
+++ app/src/main.c
@@ -4,14 +4,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include "humidity_temperature_svc.h"
+
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
+	int ret;
 
-	LOG_INF("Hello :)");
+	LOG_INF("Starting up .. .. ..");
+
+	ret = humidity_temperature_svc_init();
+	if (ret != 0) {
+		LOG_ERR("Failed to initialize humidity and temperature service!");
+	}
 
 	return 0;
 }
