e0ed860affce8392010cfe495ba5bbd058ecac35
TAREQ-TBZ <tareq@gmx.net>
2025-02-15 01:18:34 +0100

    app: trigger periodic measurements (hum. & temp.)

--- app/src/main.c
+++ app/src/main.c
@@ -10,6 +10,25 @@
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
+#define MEASUREMENT_PERIOD_MSEC      (1000 * CONFIG_MEASURING_PERIOD_SECONDS)
+#define FIRST_MEASUREMENT_DELAY_MSEC (1000 * CONFIG_FIRST_MEASUREMENT_DELAY_SECONDS)
+
+static void measuring_work_handler(struct k_work *_work)
+{
+	int ret;
+	struct k_work_delayable *work = k_work_delayable_from_work(_work);
+
+	ret = humidity_temperature_svc_trigger_measurement();
+	if (ret != 0) {
+		LOG_ERR("Failed to trigger humidity and temperature measurement: %d", ret);
+	} else {
+		/* TODO: Consume the temperature and humidity measurements in ble service */
+	}
+
+	k_work_reschedule(work, K_MSEC(MEASUREMENT_PERIOD_MSEC));
+}
+K_WORK_DELAYABLE_DEFINE(measuring_work, measuring_work_handler);
+
 int main(void)
 {
 	int ret;
@@ -20,6 +39,8 @@ int main(void)
 	if (ret != 0) {
 		LOG_ERR("Failed to initialize humidity and temperature service!");
 	}
+
+	k_work_schedule(&measuring_work, K_MSEC(CONFIG_FIRST_MEASUREMENT_DELAY_SECONDS));
 
 	return 0;
 }
